5c30d542304a178d584eec692d56055cc046666f
GImran360 <154003478+GImran360@users.noreply.github.com>
2025-09-03 10:28:27 +0530

    Update hello_airflow.py

--- dags/hello_airflow.py
+++ dags/hello_airflow.py
@@ -5,7 +5,7 @@ from datetime import datetime
 # Define a simple Python function to run
 def hello_world():
     print("✅ Hello from Airflow!")
-    print("hey hello this is imran")
+    print("hey hello this is imran okayhello")
     
     
     print("hey I am new developer for Data pipeline")
