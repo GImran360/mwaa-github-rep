50c5829448d74d58329dce26b49d8b960b3c5356
GImran360 <154003478+GImran360@users.noreply.github.com>
2025-09-04 16:49:17 +0530

    Update real_mart_data_clean_loading.py

--- dags/real_mart_data_clean_loading.py
+++ dags/real_mart_data_clean_loading.py
@@ -59,26 +59,31 @@ with DAG(
         mode='poke'
     )
 
-    # Glue jobs
-    glue_job_cart_clean = AwsGlueJobOperator(
-        task_id='glue_job_cart_clean',
+    # Step 1: User data extraction
+    glue_user_extract = AwsGlueJobOperator(
+        task_id='glue_user_extract',
+        job_name='RealMart-user_data_extract',
+        iam_role_name='realmart-iam-role',
+        region_name='ap-southeast-2',
+        script_location='s3://aws-glue-assets-258208867389-ap-southeast-2/scripts/Real-mart-user_data_extract.py'
+    )
+
+    # Step 2: Product data processing
+    glue_product_data = AwsGlueJobOperator(
+        task_id='glue_product_data',
+        job_name='Realmart-product_data_store',
+        iam_role_name='realmart-iam-role',
+        region_name='ap-southeast-2',
+        script_location='s3://aws-glue-assets-258208867389-ap-southeast-2/scripts/Realmart-Store.py'
+    )
+
+    # Step 3: Cart clean data processing
+    glue_cart_clean = AwsGlueJobOperator(
+        task_id='glue_cart_clean',
         job_name='Realmart-cart_clean_data_Store',
         iam_role_name='realmart-iam-role',
-        region_name='ap-southeast-2'
-    )
-
-    glue_job_store = AwsGlueJobOperator(
-        task_id='glue_job_store',
-        job_name='Realmart-Store',
-        iam_role_name='realmart-iam-role',
-        region_name='ap-southeast-2'
-    )
-
-    glue_job_user_extract = AwsGlueJobOperator(
-        task_id='glue_job_user_extract',
-        job_name='Real-mart-user_data_extract',
-        iam_role_name='realmart-iam-role',
-        region_name='ap-southeast-2'
+        region_name='ap-southeast-2',
+        script_location='s3://aws-glue-assets-258208867389-ap-southeast-2/scripts/Realmart-cart_clean_data_Store.py'
     )
 
     # Email on success
@@ -91,5 +96,5 @@ with DAG(
 
     # DAG flow
     holiday_check >> [skip_task, wait_for_mart_extract]
-    wait_for_mart_extract >> glue_job_cart_clean >> glue_job_store >> glue_job_user_extract >> email_success
+    wait_for_mart_extract >> glue_user_extract >> glue_product_data >> glue_cart_clean >> email_success
     skip_task >> email_success
