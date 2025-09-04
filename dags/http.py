e2a708708a3b53091d3a8c3d7e113d459c3bbc5c
GImran360 <154003478+GImran360@users.noreply.github.com>
2025-09-04 20:05:57 +0530

    http.py

--- dags/http.py
+++ dags/http.py
@@ -1,7 +1,16 @@
+from airflow import DAG
 from airflow.operators.bash import BashOperator
+from datetime import datetime
 
-test_connection = BashOperator(
-    task_id='test_fakestore_connect',
-    bash_command="curl -I https://fakestoreapi.com/products",
-    dag=dag
-)
+with DAG(
+    dag_id="mwaa_connectivity_test",
+    start_date=datetime(2025, 9, 4),
+    schedule_interval=None,
+    catchup=False,
+    tags=["test"],
+) as dag:
+
+    test_fakestore_connect = BashOperator(
+        task_id='test_fakestore_connect',
+        bash_command="curl -I https://fakestoreapi.com/products",
+    )
